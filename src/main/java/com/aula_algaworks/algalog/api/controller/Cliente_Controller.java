02abbd3b8488ef0877e946a89bf556aa9fd291e9
gabriel <gcbiotec@gmail.com>
2021-10-23 19:34:09 -0300

    CRUD

--- src/main/java/com/aula_algaworks/algalog/api/controller/Cliente_Controller.java
+++ src/main/java/com/aula_algaworks/algalog/api/controller/Cliente_Controller.java
@@ -3,15 +3,18 @@ package com.aula_algaworks.algalog.api.controller;
 import com.aula_algaworks.algalog.domain.model.Cliente;
 import com.aula_algaworks.algalog.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clientes")
 public class Cliente_Controller {
 
 //    @PersistenceContext
@@ -27,7 +30,7 @@ public class Cliente_Controller {
         this.clienteRepository = clienteRepository;
     }
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
         // Aqui está herdando as funcionalidades do JpaRepository como findAll, getById.. etc
         // Podemos também criar nosso proprios métodos la no Repository
@@ -35,6 +38,41 @@ public class Cliente_Controller {
         return clienteRepository.findAll();
 
 //      return manager.createNamedQuery("from cliente", Cliente.class).getResultList();
+    }
+
+    @GetMapping("/{clienteId}") // clienteId é path variable, precisamos fazer um binding abaixo:
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+           Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+            if (cliente.isPresent()) {
+                return ResponseEntity.ok(cliente.get());
+            }
+                return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente){
+            return clienteRepository.save(cliente);
+    }
+
+    // Obs: O método POST precisa pegar o cliente, e depois passar as novas infos;
+    // Por isso o retorno é uma ResponseEntity!
+    @PutMapping("/{clienteId}")
+    public ResponseEntity <Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente){
+            if (!clienteRepository.existsById(clienteId)){
+                return ResponseEntity.notFound().build();
+            }
+            cliente.setId(clienteId);
+            cliente = clienteRepository.save(cliente);
+            return ResponseEntity.ok(cliente);
+    }
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> deletar(@PathVariable Long clienteId){
+            if (!clienteRepository.existsById(clienteId)){
+                return ResponseEntity.notFound().build();
+            }
+            clienteRepository.deleteById(clienteId);
+            return ResponseEntity.noContent().build();
     }
 }
 //        Cliente cliente1 = new Cliente();
